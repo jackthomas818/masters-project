89f8a9742bba6ec24d344e4e31a933df4e15033b
jackthomas818 <jack.h.thomas567@gmail.com>
2022-05-09 13:07:25 -0700

    formatting code

--- code/supplementary_code_blanc.R
+++ code/supplementary_code_blanc.R
@@ -1,6 +1,6 @@
 # Supplementary Information for Blanc et al.
-# Improving abundance estimation by combining capture-recapture and occupancy data: 
-# example with a large carnivore 
+# Improving abundance estimation by combining capture-recapture and occupancy data:
+# example with a large carnivore
 
 # Load packages
 library(coda)
@@ -11,19 +11,19 @@ library(rjags)
 
 #---------------------- patch occupancy data ----------------------------#
 # Presence-absence data structure: rows = sites; columns = occasions
-y <- read.csv ('your-presence-absence-data.csv') # load presence-absence data
+y <- read.csv("your-presence-absence-data.csv") # load presence-absence data
 nsites <- dim(y)[1]
 nsurvs <- dim(y)[2]
 #--------------------- capture-recapture data -----------------------------#
 # Capture-recapture data structure : rows = individuals; columns = capture occasions
-mydata <- read.table('your-capture-recapture-data.txt') # load capture-recapture data
+mydata <- read.table("your-capture-recapture-data.txt") # load capture-recapture data
 mydata <- matrix(mydata)
-extra = 250 # define large number of extra individual capture histories
-n = nrow(mydata) # number of observed individuals
-M = extra + n
-xn = rowSums(mydata)
-x = c(xn,rep(0,extra))
-k = ncol(mydata)
+extra <- 250 # define large number of extra individual capture histories
+n <- nrow(mydata) # number of observed individuals
+M <- extra + n
+xn <- rowSums(mydata)
+x <- c(xn, rep(0, extra))
+k <- ncol(mydata)
 zerouse <- 0
 #-------------------- specify model ------------------------------#
 sink("CRandPO_HET.txt")
@@ -34,9 +34,9 @@ for (i in 1:nsites){
     truocc[i] ~ dbern(psi[i]) # true occupancy for site i
     logit(psi[i]) <- mupsi + betapsi[i] # occupancy probability as a function of μψ and a site random effect βs
     betapsi[i] ~ dnorm(0,sigmabeta)
-    
+
     logit(ppo[i]) <- logitppo[i] # pOs = site-dependant detection probability from PO model
-    logitppo[i] ~ dnorm(mean.p0,sigmaeps)    
+    logitppo[i] ~ dnorm(mean.p0,sigmaeps)
 
  # observation model
   for (t in 1:nsurvs) {
@@ -44,7 +44,7 @@ for (i in 1:nsites){
     effp[i,t] <- truocc[i]*ppo[i]
   }
 }
-    
+
     # Capture-recapture (CR) likelihood
  for(i in 1:M){
     x[i] ~ dbin(pee[i],k)
@@ -52,48 +52,46 @@ for (i in 1:nsites){
     logit(pcr[i]) <- mumup + xi*etap[i] # integration of an individual random effect ηi
     etap[i] ~ dnorm(0,sigmeta)
  }
-    
+
     N ~ dpois(lamn)T(0,M)
     psi0 <- 1/(1+exp(-mupsi))  # μψ back transformed
     log(lamn) <- log(-log(1-psi0)) # definition of λ as a function of ψ
     mumup0 <- 1/(1+exp(-mumup)) # μp back transformed
     zerouse ~ dpois(comb)
     comb <- logfact(M)-logfact(N)+logfact((N-n)*step(N-n))
-    
+
     # priors for occupancy probability
-    mupsi ~ dnorm(0,0.2) 
-    sigmabeta <- 1/(sdbeta*sdbeta) 
-    sdbeta~ dunif(0,5) 
+    mupsi ~ dnorm(0,0.2)
+    sigmabeta <- 1/(sdbeta*sdbeta)
+    sdbeta~ dunif(0,5)
 
      # priors for detection probability in PO likelihood
-    mean.p0 ~ dnorm(0,0.1) 
-    sigmaeps <- 1/(sdeps*sdeps) 
-    sdeps ~ dunif(0,10) 
-    
+    mean.p0 ~ dnorm(0,0.1)
+    sigmaeps <- 1/(sdeps*sdeps)
+    sdeps ~ dunif(0,10)
+
      # priors for detection probability in CR likelihood
     mumup ~ dlogis(0,1)
     sigmeta ~ dgamma(1.5,37.5)
-    xi ~ dnorm(0,1) 
-} 
-    ",fill=TRUE)
+    xi ~ dnorm(0,1)
+}
+    ", fill = TRUE)
 sink()
 
 #-------------------- Jags stuff -----------------------#
 # List of data
-mydatax <- list (x=x,M=M,n=n,k=k,zerouse=zerouse,y=y,nsites=nsites,nsurvs=nsurvs)
+mydatax <- list(x = x, M = M, n = n, k = k, zerouse = zerouse, y = y, nsites = nsites, nsurvs = nsurvs)
 
 # Parameters monitored
-parameters = c ('N', 'ppo', 'mumup0', 'psi0', 'mupsi', 'mean.p0', 'sdeps', 'taueps', 'betapsi', 'etap')
+parameters <- c("N", "ppo", "mumup0", "psi0", "mupsi", "mean.p0", "sdeps", "taueps", "betapsi", "etap")
 
 
 # Initial values
-init1 <- list(mupsi=runif(1),truocc=as.numeric(apply(y,1,sum)>0),xi = rnorm(1,sd=2), N = sample(n:M,1),mumup = rnorm(1), taup = rlnorm(1))
-init2 <- list(mupsi=runif(1),truocc=as.numeric(apply(y,1,sum)>0),xi = rnorm(1,sd=2), N = sample(n:M,1),mumup = rnorm(1), taup = rlnorm(1))
-init3 <- list(mupsi=runif(1),truocc=as.numeric(apply(y,1,sum)>0),xi = rnorm(1,sd=2), N = sample(n:M,1),mumup = rnorm(1), taup = rlnorm(1))
-inits <- list(init1,init2,init3)
+init1 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
+init2 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
+init3 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
+inits <- list(init1, init2, init3)
 
 #-------------------- Call jags from R -----------------------#
-jmodel <- jags.model("CRandPO_HET.txt", mydatax, inits, n.chains = 3,n.adapt = 2500)
-jsample <- coda.samples(jmodel, parameters, n.iter=15000, thin = 1)
-
-
+jmodel <- jags.model("CRandPO_HET.txt", mydatax, inits, n.chains = 3, n.adapt = 2500)
+jsample <- coda.samples(jmodel, parameters, n.iter = 15000, thin = 1)
