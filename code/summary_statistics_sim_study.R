91d20ae79120c227acaba84f847a21e1b541f138
jackthomas818 <jack.h.thomas567@gmail.com>
2022-07-20 13:04:05 -0700

    data read into seperate tables

--- code/summary_statistics_sim_study.R
+++ code/summary_statistics_sim_study.R
@@ -2,9 +2,32 @@
 library(readr)
 library(tictoc)
 
-
 tic()
 
 print("finished loading libraries")
+
+# get command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# directory of simulation study
+directory <- args[1]
+
+# Getting presence-absence data
+pa_files <- list.files(directory, pattern = "presence-absence")
+pa_data <- lapply(paste0(directory, "\\", pa_files), function(i) {
+  read.csv(i, header = FALSE)
+})
+
+# Getting capture-recapture data
+cr_files <- list.files(directory, pattern = "capture-recapture")
+cr_data <- lapply(paste0(directory, "\\", cr_files), function(i) {
+  read.table(i, header = FALSE)
+})
+
+# Getting model statistics
+model_files <- list.files(directory, pattern = "model_statistics")
+model_data <- lapply(paste0(directory, "\\", model_files), function(i) {
+  read.table(i, header = TRUE)
+})
 
 toc()
