40ca0336b4ae0099b160f590206457532614d316
jackthomas818 <jack.h.thomas567@gmail.com>
2022-07-27 12:15:30 -0700

    means calculated by simulation number

--- code/summary_statistics_sim_study.R
+++ code/summary_statistics_sim_study.R
@@ -1,6 +1,7 @@
 # Load packages
 library(readr)
 library(tictoc)
+library(data.table)
 
 tic()
 
@@ -12,7 +13,7 @@ args <- commandArgs(trailingOnly = TRUE)
 # directory of simulation study
 directory <- args[1]
 
-#directory <- "testSimStudy/"
+#directory <- "SimStudy4/"
 
 # Getting presence-absence data
 pa_files <- list.files(directory, pattern = "presence-absence")
@@ -31,24 +32,35 @@ params_matrix <- read.csv("./parameter_matrix.csv")
 
 # Getting model statistics
 # construct summary statistics table
-N_mean <- array(data=NA, dim=nrow(params_matrix))
-N_lower_95 <- array(data=NA, dim=nrow(params_matrix)) 
-N_upper_95 <- array(data=NA, dim=nrow(params_matrix)) 
-n_captured <- array(data=NA, dim=nrow(params_matrix)) 
+N_mean <- array(data = NA, dim = nrow(params_matrix))
+N_lower_95 <- array(data = NA, dim = nrow(params_matrix))
+N_upper_95 <- array(data = NA, dim = nrow(params_matrix))
+n_captured <- array(data = NA, dim = nrow(params_matrix))
 
-for(task_id in 1:nrow(params_matrix)){
-  model_data <- read.table(paste0(directory,"/model_statistics_",task_id,".txt"))
-  cr_data <- read.table(paste0(directory,"/capture-recapture-data-",task_id,".txt"))
+for (task_id in 1:nrow(params_matrix)) {
+  model_data <- read.table(paste0(directory, "/model_statistics_", task_id, ".txt"))
+  cr_data <- read.table(paste0(directory, "/capture-recapture-data-", task_id, ".txt"))
 
   N_mean[task_id] <- unlist(model_data["Mean"])[1]
   N_lower_95[task_id] <- unlist(model_data["X2.5."])[1]
   N_upper_95[task_id] <- unlist(model_data["X97.5."])[1]
-  n_captured[task_id] <- nrow(cr_data)  
-  
+  n_captured[task_id] <- nrow(cr_data)
 }
 
 
-all_data <- cbind(params_matrix,N_mean,n_captured, N_lower_95,N_upper_95)
+all_data <- cbind(params_matrix, N_mean, n_captured, N_lower_95, N_upper_95)
 
+
+# Calculate means by simulation number
+setDT(all_data)
+
+all_data[, list(
+  N_actual=mean(N),
+  n_captured = mean(n_captured),
+  tau=mean(tau),
+  N_mean = mean(N_mean),
+  lower_95_N = mean(N_lower_95),
+  upper_95_N = mean(N_upper_95)
+), by = sim]
 
 toc()
