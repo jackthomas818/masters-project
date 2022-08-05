cf2a6f06c91980ae53f05d43039317bceb0295e1
jackthomas818 <jack.h.thomas567@gmail.com>
2022-08-05 10:57:39 -0700

    added presence detection totals to summary statistics

--- code/summary_statistics_sim_study.R
+++ code/summary_statistics_sim_study.R
@@ -15,12 +15,6 @@ directory <- args[1]
 
 #directory <- "SimStudy4"
 
-# Getting capture-recapture data
-cr_files <- list.files(directory, pattern = "capture-recapture")
-cr_data <- lapply(paste0(directory, "\\", cr_files), function(i) {
-  read.table(i, header = FALSE)
-})
-
 # parameter matrix from generate_parameter_matrix.R
 params_matrix <- read.csv("./parameter_matrix.csv")
 
@@ -33,10 +27,16 @@ n_captured <- array(data = NA, dim = nrow(params_matrix))
 N_sd <- array(data = NA, dim = nrow(params_matrix))
 N_naive_se <- array(data = NA, dim = nrow(params_matrix))
 
+pa_detects_total <- array(data = NA, dim = nrow(params_matrix))
+
 for (task_id in 1:nrow(params_matrix)) {
   model_data <- read.table(paste0(directory, "/model_statistics_", task_id, ".txt"))
 
   cr_data <- read.table(paste0(directory, "/capture-recapture-data-", task_id, ".txt"))
+  pa_data <- read.csv(paste0(directory, "/presence-absence-data-", task_id, ".csv"), header = FALSE)
+
+  # count number of presence detections
+  pa_detects_total[task_id] <- sum(rowSums(pa_data))
 
   N_mean[task_id] <- unlist(model_data["Mean"])[1]
   N_lower_95[task_id] <- unlist(model_data["X2.5."])[1]
@@ -46,8 +46,7 @@ for (task_id in 1:nrow(params_matrix)) {
   n_captured[task_id] <- nrow(cr_data)
 }
 
-
-all_data <- cbind(params_matrix, N_mean, n_captured, N_lower_95, N_upper_95, N_sd, N_naive_se)
+all_data <- cbind(params_matrix, N_mean, n_captured, pa_detects_total, N_lower_95, N_upper_95, N_sd, N_naive_se)
 
 
 # Calculate means by simulation number
@@ -56,16 +55,17 @@ setDT(all_data)
 all_data[, list(
   N_actual = mean(N),
   n_captured = mean(n_captured),
+  pa_detects = mean(pa_detects_total),
   tau = mean(tau),
   N_mean = mean(N_mean),
   N_sd = mean(N_sd),
-  N_naive_se=mean(N_naive_se),
+  N_naive_se = mean(N_naive_se),
   lower_95_N = mean(N_lower_95),
   upper_95_N = mean(N_upper_95)
 ), by = sim]
 
 
 # Output summary statistics
-write.csv(all_data,file=paste0("summary_stats_",directory,".csv"))
+write.csv(all_data, file = paste0("summary_stats_", directory, ".csv"))
 
 toc()
