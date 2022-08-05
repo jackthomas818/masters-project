4b2610df686ce5cbf316cf2c31e325993572e3a4
jackthomas818 <jack.h.thomas567@gmail.com>
2022-08-05 13:21:25 -0700

    removed reps from generate_parameter_matrix.R, added reps as argument to runsim.R, seed is generated each rep and appended to model_statistics filename. submitScript now has reps in the arguments

--- code/runsim.R
+++ code/runsim.R
@@ -24,6 +24,9 @@ args <- commandArgs(trailingOnly = TRUE)
 task_id_str <- args[1]
 task_id <- strtoi(args[1])
 
+# number of repetitions for each simulation
+reps <- args[2]
+
 print(paste0("task id: ", task_id_str))
 
 # parameter matrix from generate_parameter_matrix.R
@@ -53,62 +56,68 @@ tau <- as.double(params["tau"])
 # number of camera traps
 ntraps <- as.integer(params["ntraps"])
 
-# setting seed
-set.seed(as.integer(params["seed"]))
+for (rep in 1:reps) {
+  # setting seed
+  seed <- as.integer(1e7 * runif(1))
+  set.seed(seed)
 
-# homeranges for the n individuals
-homeranges <- create_homeranges(n_total)
+  print(paste0("seed: ", seed))
 
-capture_hist <- generate_capture_hist(ntraps, nsample_cap, tau, homeranges)
-capture_hist
+  # homeranges for the n individuals
+  homeranges <- create_homeranges(n_total)
 
-presence_absence <- generate_presence_absence(nsites, nsample_pa, tau, homeranges)
-presence_absence
+  capture_hist <- generate_capture_hist(ntraps, nsample_cap, tau, homeranges)
+  capture_hist
 
-output_data(presence_absence, capture_hist, task_id_str)
+  presence_absence <- generate_presence_absence(nsites, nsample_pa, tau, homeranges)
+  presence_absence
 
-# Supplementary Code by Blanc et al. (2014)
+  output_data(presence_absence, capture_hist, paste0(task_id_str,"-",rep))
 
-#---------------------- patch occupancy data ----------------------------#
-# Presence-absence data structure: rows = sites; columns = occasions
-y <- read.csv(paste0("./presence-absence-data-", task_id_str, ".csv"), header = FALSE) # load presence-absence data
-nsites <- dim(y)[1]
-nsurvs <- dim(y)[2]
-#--------------------- capture-recapture data -----------------------------#
-# Capture-recapture data structure : rows = individuals; columns = capture occasions
-mydata <- read.table(paste0("./capture-recapture-data-", task_id_str, ".txt"), header = FALSE) # load capture-recapture data
-extra <- 250 # define large number of extra individual capture histories
-n <- nrow(mydata) # number of observed individuals
-M <- extra + n
-xn <- rowSums(mydata)
-x <- c(xn, rep(0, extra))
-k <- ncol(mydata)
-zerouse <- 0
+  # Supplementary Code by Blanc et al. (2014)
 
-#-------------------- Jags stuff -----------------------#
-# List of data
-mydatax <- list(x = x, M = M, n = n, k = k, zerouse = zerouse, y = y, nsites = nsites, nsurvs = nsurvs)
+  #---------------------- patch occupancy data ----------------------------#
+  # Presence-absence data structure: rows = sites; columns = occasions
+  y <- read.csv(paste0("./presence-absence-data-", task_id_str, ".csv"), header = FALSE) # load presence-absence data
+  nsites <- dim(y)[1]
+  nsurvs <- dim(y)[2]
+  #--------------------- capture-recapture data -----------------------------#
+  # Capture-recapture data structure : rows = individuals; columns = capture occasions
+  mydata <- read.table(paste0("./capture-recapture-data-", task_id_str, ".txt"), header = FALSE) # load capture-recapture data
+  extra <- 250 # define large number of extra individual capture histories
+  n <- nrow(mydata) # number of observed individuals
+  M <- extra + n
+  xn <- rowSums(mydata)
+  x <- c(xn, rep(0, extra))
+  k <- ncol(mydata)
+  zerouse <- 0
 
-# Parameters monitored
-parameters <- c("N", "ppo", "mumup0", "psi0", "mupsi", "mean.p0", "sdeps", "taueps", "betapsi", "etap")
+  #-------------------- Jags stuff -----------------------#
+  # List of data
+  mydatax <- list(x = x, M = M, n = n, k = k, zerouse = zerouse, y = y, nsites = nsites, nsurvs = nsurvs)
+
+  # Parameters monitored
+  parameters <- c("N", "ppo", "mumup0", "psi0", "mupsi", "mean.p0", "sdeps", "taueps", "betapsi", "etap")
 
 
-# Initial values
-init1 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
-init2 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
-init3 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
-inits <- list(init1, init2, init3)
+  # Initial values
+  init1 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
+  init2 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
+  init3 <- list(mupsi = runif(1), truocc = as.numeric(apply(y, 1, sum) > 0), xi = rnorm(1, sd = 2), N = sample(n:M, 1), mumup = rnorm(1), taup = rlnorm(1))
+  inits <- list(init1, init2, init3)
 
-#-------------------- Call jags from R -----------------------#
-jmodel <- jags.model("CRandPO_HET.txt", mydatax, inits, n.chains = 3, n.adapt = 2500)
-jsample <- coda.samples(jmodel, parameters, n.iter = 15000, thin = 1)
-save(jsample, file = paste0("coda_samples_", task_id))
+  #-------------------- Call jags from R -----------------------#
+  jmodel <- jags.model("CRandPO_HET.txt", mydatax, inits, n.chains = 3, n.adapt = 2500)
+  jsample <- coda.samples(jmodel, parameters, n.iter = 15000, thin = 1)
+  save(jsample, file = paste0("coda_samples_", task_id))
 
-# Get summary statistics
-s <- summary(jsample)
+  # Get summary statistics
+  s <- summary(jsample)
 
-all_table <- cbind(s$statistics, s$quantiles)
+  all_table <- cbind(s$statistics, s$quantiles)
 
-write.table(all_table, file = paste0("model_statistics_", task_id, ".txt"))
+  write.table(all_table, file = paste0("model_statistics_", task_id_str, "_", toString(rep), "_", toString(seed), ".txt"))
+}
+
 
 print(tictoc::toc())
