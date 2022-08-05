0914b40815ec8900e8b31009db313676ad2cfe6d
jackthomas818 <jack.h.thomas567@gmail.com>
2022-08-05 14:24:27 -0700

    add seed to model statistics output file, make summary statistics read new file structure

--- code/summary_statistics_sim_study.R
+++ code/summary_statistics_sim_study.R
@@ -13,55 +13,63 @@ args <- commandArgs(trailingOnly = TRUE)
 # directory of simulation study
 directory <- args[1]
 
-# directory <- "SimStudy5"
+# number of repetitions per simulation
+reps <- strtoi(args[2])
+
+#directory <- "SimStudy6"
+#reps <- 1
 
 # parameter matrix from generate_parameter_matrix.R
 params_matrix <- read.csv(paste0(directory, "/parameter_matrix.csv"))
 
 # Getting model statistics
 # construct summary statistics table
-N_mean <- array(data = NA, dim = nrow(params_matrix))
-N_lower_95 <- array(data = NA, dim = nrow(params_matrix))
-N_upper_95 <- array(data = NA, dim = nrow(params_matrix))
-n_captured <- array(data = NA, dim = nrow(params_matrix))
-N_sd <- array(data = NA, dim = nrow(params_matrix))
-N_naive_se <- array(data = NA, dim = nrow(params_matrix))
+N_mean <- array(data = NA, dim = nrow(params_matrix) * reps)
+N_lower_95 <- array(data = NA, dim = nrow(params_matrix) * reps)
+N_upper_95 <- array(data = NA, dim = nrow(params_matrix) * reps)
+n_captured <- array(data = NA, dim = nrow(params_matrix) * reps)
+N_sd <- array(data = NA, dim = nrow(params_matrix) * reps)
+N_naive_se <- array(data = NA, dim = nrow(params_matrix) * reps)
 
 pa_detects_total <- array(data = NA, dim = nrow(params_matrix))
 
+count <- 1
 for (task_id in 1:nrow(params_matrix)) {
-  if (task_id %% 100 == 0) {
-    print(paste0(task_id, " out of ", nrow(params_matrix)))
-  }
+  for (rep in 1:reps) {
+    if (task_id %% 100 == 0) {
+      print(paste0(task_id, " out of ", nrow(params_matrix)))
+    }
 
-  model_file <- paste0(directory, "/model_statistics_", task_id, ".txt")
-  if (file.exists(model_file)) {
-    model_data <- tryCatch(read.table(model_file), error = function(e) NULL)
-  }
+    model_file <- paste0(directory, "/model_statistics_", task_id, "_", rep, ".txt")
+    if (file.exists(model_file)) {
+      model_data <- tryCatch(read.table(model_file), error = function(e) NULL)
+    }
 
-  cr_file <- paste0(directory, "/capture-recapture-data-", task_id, ".txt")
-  if (file.exists(cr_file)) {
-    cr_data <- tryCatch(read.table(cr_file), error = function(e) NULL)
-  }
+    cr_file <- paste0(directory, "/capture-recapture-data-", task_id, "-", rep, ".txt")
+    if (file.exists(cr_file)) {
+      cr_data <- tryCatch(read.table(cr_file), error = function(e) NULL)
+    }
 
-  pa_file <- paste0(directory, "/presence-absence-data-", task_id, ".csv")
-  if (file.exists(pa_file)) {
-    pa_data <- tryCatch(read.csv(pa_file, header = FALSE), error = function(e) NULL)
-  }
+    pa_file <- paste0(directory, "/presence-absence-data-", task_id, "-", rep, ".csv")
+    if (file.exists(pa_file)) {
+      pa_data <- tryCatch(read.csv(pa_file, header = FALSE), error = function(e) NULL)
+    }
 
-  # count number of presence detections
-  pa_detects_total[task_id] <- sum(rowSums(pa_data))
+    # count number of presence detections
+    pa_detects_total[count] <- sum(rowSums(pa_data))
 
-  N_mean[task_id] <- unlist(model_data["Mean"])[1]
-  N_lower_95[task_id] <- unlist(model_data["X2.5."])[1]
-  N_upper_95[task_id] <- unlist(model_data["X97.5."])[1]
-  N_sd[task_id] <- unlist(model_data["SD"])[1]
-  N_naive_se[task_id] <- unlist(model_data["Naive.SE"])[1]
+    N_mean[count] <- unlist(model_data["Mean"])[1]
+    N_lower_95[count] <- unlist(model_data["X2.5."])[1]
+    N_upper_95[count] <- unlist(model_data["X97.5."])[1]
+    N_sd[count] <- unlist(model_data["SD"])[1]
+    N_naive_se[count] <- unlist(model_data["Naive.SE"])[1]
 
-  if (is.null(cr_data)) {
-    n_captured[task_id] <- 0
-  } else {
-    n_captured[task_id] <- nrow(cr_data)
+    if (is.null(cr_data)) {
+      n_captured[count] <- 0
+    } else {
+      n_captured[count] <- nrow(cr_data)
+    }
+    count <- count + 1
   }
 }
 
