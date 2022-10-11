e17060135d1119e771bed430c05601d2599db330
jackthomas818 <jack.h.thomas567@gmail.com>
2022-10-11 12:45:49 -0700

    added papers to resources

--- code/generate_parameter_matrix.R
+++ code/generate_parameter_matrix.R
@@ -8,12 +8,12 @@ debug <- FALSE
 
 ## Define simulation parameters
 pars_mat <- crossing(
-  N = c(10, 25, 50,75,100,125,150,175,200),
+  N = c(50,200),
   tau = c(0.15,0.20,0.25,0.30,0.35,0.4,0.45,0.5,0.55),
-  nsites = 4**2,
-  nsample_cap = 7,
-  nsample_pa = 5,
-  ntraps = c(3,4,5)
+  nsites = c(7**2,8**2,9**2,10**2),
+  nsample_cap = c(8,10,12,14),
+  nsample_pa = c(8,10,12,14),
+  ntraps = c(3,4,5,6)
 ) %>%
   rowid_to_column("sim") %>%
   rowid_to_column("task")
