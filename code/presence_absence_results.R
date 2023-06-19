059146b55cb9aa2ccaa47aabd32a2cd383683962
Simon Bonner <sbonner6@uwo.ca>
2023-06-19 15:58:18 -0400

    Added new plot exploring the relationship between the presence-absence data and population size estimate.

---
+++ code/presence_absence_results.R
@@ -0,0 +1,27 @@
+# Load packages
+library(tidyverse)
+library(latex2exp)
+
+# Load simulation summaries
+sim12 <- read_csv("summary_stats_SimStudy12.csv") %>%
+  mutate(s_d = sigma * delta)
+
+# Parameter summaries
+sim12 %>% 
+  group_by(N_actual, tau, nsites, ntraps, sigma, delta) %>%
+  summarize(n = n())
+
+# Plot estimates by presence-absence detects
+sim12 %>%
+  filter(N_actual == 500) %>%
+  mutate(p = factor(p)) %>%
+  ggplot(aes(x = s_d, y = N_mean, colour = p)) + 
+  geom_point() + 
+  facet_grid(tau ~ .) + 
+  geom_hline(aes(yintercept = N_actual)) +
+  xlab(TeX("$\\sigma\\delta$")) +
+  ylab("Mean Estimated Population Size")
+
+dev.copy2pdf(file="Figures/presence_absence_1.pdf", width = 6, height = 5)
+dev.off()
+
