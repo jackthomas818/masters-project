3e5f402eeb40f0117988bc181a08b02fb9b43c40
sjbonner <sbonner6@uwo.ca>
2024-04-04 13:53:07 -0400

    Created separate scripts to generate parameter matrices for the two simulation scenarios.

---
+++ code/generate_parameter_matrix_B.R
@@ -0,0 +1,24 @@
+## Load packages
+library(tibble)
+library(tidyr)
+library(readr)
+
+## Set simulation parameters
+debug <- FALSE
+
+## Define simulation parameters
+pars_mat <- crossing(
+  N = c(10, 500),
+  tau = c(0.15, 0.20, 0.25, 0.30, 0.35, 0.4, 0.45, 0.5, 0.55),
+  nsites = 8**2,
+  nsample_cap = 7,
+  nsample_pa = 5,
+  ntraps = 5,
+  p = c(0.1, 0.25, 0.5, 0.75),
+  sigma = c(1, 2, 3, 4),
+  delta = c(1, 2, 3, 4)
+) %>%
+  rowid_to_column("sim") %>%
+  rowid_to_column("task")
+
+write_csv(pars_mat, "./parameter_matrix.csv")
